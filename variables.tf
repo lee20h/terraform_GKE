753f78197447fc92847b4108f06181a552c3d607
Young Hun <lee0h.0417@gmail.com>
2021-08-02 22:28:12 +0900

    Terraform With GKE

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "project_id" {
+  description = "project id"
+}
+
+variable "region" {
+  description = "region"
+}
+
+variable "gke_num_nodes" {
+  default     = 3
+  description = "number of gke nodes"
+}
+
+variable "gke_machine_type" {
+  default     = "e2-medium"
+  description = "machine type of gke nodes"
+}
