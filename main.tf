61bb48dcad53eef972b375cd07466a073cc88fd9
Young Hun <lee0h.0417@gmail.com>
2021-09-05 19:22:32 +0900

    Add Terraform Backend

--- main.tf
+++ main.tf
@@ -4,6 +4,13 @@ provider "google" {
   region  = var.region
 }
 
+terraform {
+  backend "gcs" {
+    bucket  = "my-tfstate"
+    prefix  = "gke"
+  }
+}
+
 # VPC
 resource "google_compute_network" "vpc" {
   name                    = "${var.project_id}-vpc"
